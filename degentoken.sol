98bd71ba8a4299078120d14c8c8035ad3dd8c5a3
Navdeep <108007267+nxvtej@users.noreply.github.com>
2023-08-13 16:12:05 +0530

    Update degentoken.sol

--- degentoken.sol
+++ degentoken.sol
@@ -1,1 +1,40 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract DEGENTOKEN is ERC20 {
+    address private contractOwner;
+
+    constructor() ERC20("DEGEN TOKEN", "DGN") {
+        contractOwner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner, "Restricted to the contract owner only");
+        _;
+    }
+    
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function redeemTokens(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function checkAccountBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
